c9ee46f604d21da4ea3a893e118d7c73ba90314f
HashimsGitHub <79075502+HashimsGitHub@users.noreply.github.com>
2025-12-08 19:50:55 +1000

    Create setup script for Azure CLI and Python environment
    This script installs Python, sets up a virtual environment, installs necessary packages, clones a repository, and runs a Python script for Azure login.

---
+++ ai102prep.sh
@@ -0,0 +1,16 @@
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install python3.10-venv -y
+python3 -m venv labenv
+source labenv/bin/activate
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+pip install azure-identity agent-framework
+pip install python-dotenv
+
+git clone https://github.com/MicrosoftLearning/mslearn-ai-agents ai-agents
+
+cd ai-agents/Labfiles/05-agent-orchestration/Python
+
+python3 azurelogin.py
